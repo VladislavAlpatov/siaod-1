d99d0ba6f4d4695f6e52efb37c6eea06cca9b579
Vladislav <alpatof74@gmail.com>
2023-03-06 15:29:55 +0300

    added prob-3

---
+++ prob-3/main.cpp
@@ -0,0 +1,40 @@
+//
+// Created by vladislav on 06.03.23.
+//
+#include "utils.h"
+#include <regex>
+#include <iostream>
+
+std::vector<std::string> Split(const std::string& str)
+{
+    std::vector<std::string> substrings;
+    std::regex re("\\s+");
+    std::sregex_token_iterator it(str.begin(), str.end(), re, -1);
+    std::sregex_token_iterator end;
+
+    for (; it != end; ++it)
+        substrings.push_back(*it);
+
+    return substrings;
+}
+
+int main()
+{
+    std::vector<int> parsedNumbers;
+    std::string  text;
+
+    while (true)
+    {
+        printf("Enter text: ");
+        std::getline(std::cin, text);
+
+        for (const auto& subStr : Split(text))
+            parsedNumbers.push_back(ParseNumber(subStr));
+
+        for (const auto& number : parsedNumbers)
+            printf("%d ", number);
+
+        printf("\n");
+    }
+
+}
---
+++ prob-3/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by vladislav on 06.03.23.
+//
+#pragma once
+#include <string>
+
+
+int ParseNumber(const std::string& str);
