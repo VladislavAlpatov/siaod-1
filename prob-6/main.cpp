30efda6c3dfe6949a9d560f01c81b250d1cf4a55
Vladislav <alpatof74@gmail.com>
2023-04-13 21:18:51 +0300

    fix 6

--- prob-6/main.cpp
+++ prob-6/main.cpp
@@ -2,17 +2,19 @@
 // Created by alpat on 29.03.2023.
 //
 #include <string>
+#include <iostream>
+#include "LinkedList.h"
 
 
-bool isBalanced(const std::string& s, int index = 0, int open = 0)
+bool isBalanced(LinkedList<char>& s, int index = 0, int open = 0)
 {
-    if (index == s.size())
+    if (index == s.Size())
         return open == 0;
 
-    if (s[index] == '(')
+    if (s.At(index) == '(')
         return isBalanced(s, index + 1, open + 1);
 
-    if (s[index] != ')')
+    if (s.At(index) != ')')
         return isBalanced(s, index + 1, open);
 
     if (open > 0)
@@ -34,19 +36,59 @@ bool CheckIfDevBySelfNumber(int iNumber)
     }
     return true;
 }
-bool CheckIfDevBySelfNumberRecursive(int iNumber)
+bool CheckIfDevBySelfNumberRecursive(int originalNum, int num)
 {
-    if (!iNumber)
-        return true;
-    auto iLastDigit = iNumber % 10;
+	auto iLast = num % 10;
+	num /= 10;
 
-    if (!iLastDigit or iNumber % iLastDigit)
-        return false;
+	if (num == 0)
+		return true;
 
-    return CheckIfDevBySelfNumberRecursive(iNumber / 10);
+	if (iLast == 0 or originalNum % iLast)
+		return false;
+
+	if(CheckIfDevBySelfNumberRecursive(originalNum, num))
+		return true;
+
+	return false;
 }
 int main()
 {
-    if (CheckIfDevBySelfNumberRecursive(93665))
-        printf("ok\n");
+	while (true)
+	{
+		printf("\n0 - Check balance of bracket\n1 - Check self number digits divisibility\nChoose operation: ");
+		int iMode;
+		std::cin >> iMode;
+
+		if (iMode == 0)
+		{
+			std::string exrp;
+			printf("Enter mathematics expression: ");
+			std::cin >> exrp;
+			LinkedList<char> tmp;
+
+			for (auto chr : exrp)
+				tmp.PushBack(chr);
+
+			(isBalanced(tmp)) ? printf("Expression is balanced\n") : printf("Expression is not balanced\n");
+			continue;
+		}
+		int val = 0;
+		int iRecursive;
+		printf("Enter value: ");
+		std::cin >> val;
+		printf("Use recursive algorithm? [0 - no | 1 - yes]: ");
+		std::cin >> iRecursive;
+
+		if ((iRecursive) ? CheckIfDevBySelfNumberRecursive(val, val) : CheckIfDevBySelfNumber(val))
+		{
+			printf("All self digits of number dived it!\n");
+		}
+		else
+		{
+			printf("Nope :(\n");
+		}
+
+	}
+
 }
